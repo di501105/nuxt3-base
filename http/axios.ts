9574215b540d057bfdc2070bc3a894c8b0b06262
Di Tseng <shengdi.tseng@gmail.com>
2025-03-25 21:24:49 +0800

    chore: axios

--- http/axios.ts
+++ http/axios.ts
@@ -1,37 +1,39 @@
 import type { InternalAxiosRequestConfig } from 'axios'
-import instance from './index'
+import instance from './config'
 /**
- * @param {String} method  請求的方法：get、post、delete、put
- * @param {String} url     請求的url:
- * @param {Object} data    請求的参数
- * @param {Object} config  請求的配置
+ * @param {string} method  請求的方法：get、post、delete、put
+ * @param {string} url     請求的url:
+ * @param {object} data    請求的参数
+ * @param {object} config  請求的配置
  * @returns {Promise}     返回一個promise對象，其實就相當於axios請求數據的返回值
  */
-interface IAxios {
-  method: string
+export interface IAxiosRequest {
+  method: 'post' | 'get' | 'delete' | 'put'
   url: string
-  data?: object | undefined
+  data?: Record<string, any>
   config?: InternalAxiosRequestConfig
 }
-const axios = ({ method, url, data, config }: IAxios) => {
-  method = method.toLowerCase()
+
+export interface IAxiosResponse<T = any> {
+  code: '0000' | '9999'
+  message: string
+  data: T
+}
+
+function axios<T = any>({ method, url, data, config }: IAxiosRequest) {
   if (method === 'post') {
-    return instance.post(url, data, { ...config }) as any
-  } else if (method === 'get') {
-    return instance.get(url, {
-      params: data,
-      ...config
-    })
-  } else if (method === 'delete') {
-    return instance.delete(url, {
-      params: data,
-      ...config
-    })
-  } else if (method === 'put') {
-    return instance.put(url, data, { ...config })
-  } else if (method === 'patch') {
-    return instance.put(url, data, { ...config })
-  } else {
+    return instance.post<IAxiosResponse<T>>(url, data, config)
+  }
+  else if (method === 'get') {
+    return instance.get<IAxiosResponse<T>>(url, { params: data, ...config })
+  }
+  else if (method === 'delete') {
+    return instance.delete<IAxiosResponse<T>>(url, { params: data, ...config })
+  }
+  else if (method === 'put') {
+    return instance.put<IAxiosResponse<T>>(url, data, config)
+  }
+  else {
     return undefined
   }
 }
